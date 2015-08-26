ea3933f11af6e264bdb29c946e6b22183c8e32af
Grabacr07 <grabacr07+github@gmail.com>
2015-08-27 04:59:11 +0900

    Add comment.

--- source/VirtualDesktop/Interop/IVirtualDesktopManager.cs
+++ source/VirtualDesktop/Interop/IVirtualDesktopManager.cs
@@ -14,4 +14,9 @@ namespace WindowsDesktop.Interop
 
 		void MoveWindowToDesktop(IntPtr topLevelWindow, ref Guid desktopId);
 	}
+
+	// see also: 
+	//  "C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\um\ShObjIdl.h"
+	//  https://msdn.microsoft.com/en-us/library/windows/desktop/mt186440%28v=vs.85%29.aspx
+	//  http://www.cyberforum.ru/blogs/105416/blog3671.html
 }
