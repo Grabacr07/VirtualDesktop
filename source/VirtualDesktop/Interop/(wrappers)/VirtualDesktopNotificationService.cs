3f99e7029e7bb058b1c4283bc0a4ab7f2f621a52
Grabacr07 <grabacr07+github@gmail.com>
2018-05-28 06:27:45 +0900

    Fix crash when explorer.exe restarted.

--- source/VirtualDesktop/Interop/(wrappers)/VirtualDesktopNotificationService.cs
+++ source/VirtualDesktop/Interop/(wrappers)/VirtualDesktopNotificationService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using WindowsDesktop.Internal;
 
 namespace WindowsDesktop.Interop
@@ -19,7 +20,13 @@ namespace WindowsDesktop.Interop
 
 		private void Unregister(uint dwCookie)
 		{
-			this.Invoke(Args(dwCookie));
+			try
+			{
+				this.Invoke(Args(dwCookie));
+			}
+			catch (COMException ex) when (ex.Match(HResult.RPC_S_SERVER_UNAVAILABLE))
+			{
+			}
 		}
 	}
 }
