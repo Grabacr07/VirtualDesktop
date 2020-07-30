d3eb4e3fe73415b5390b3ee242edc81bfacbc606
mntone <sd8@live.jp>
2020-07-30 23:02:34 +0900

    Implement INotifyPropertyChanging/ed to VirtualDesktop.

---
+++ source/VirtualDesktop/VirtualDesktop.notification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace WindowsDesktop
+{
+	partial class VirtualDesktop : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Occurs when a virtual desktop property changing.
+		/// </summary>
+		public event PropertyChangingEventHandler PropertyChanging;
+
+		private void RaisePropertyChanging([CallerMemberName] string propertyName = "")
+			=> PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+
+		/// <summary>
+		/// Occurs when a virtual desktop property changed.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+			=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
