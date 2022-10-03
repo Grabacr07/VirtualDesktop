3e486193d48963c920ec67bf557d80df5949128c
Maciej Makowski <377311+maciej-makowski@users.noreply.github.com>
2022-10-03 22:22:54 +0100

    Fixing crash on Windows > 22000 (#76)

--- src/VirtualDesktop/Interop/Build22000/.Provider.cs
+++ src/VirtualDesktop/Interop/Build22000/.Provider.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using WindowsDesktop.Interop.Build10240;
+﻿using WindowsDesktop.Interop.Build10240;
 using WindowsDesktop.Interop.Proxy;
-using WindowsDesktop.Properties;
 
 namespace WindowsDesktop.Interop.Build22000;
 
